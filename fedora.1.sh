0abb184ad349c5cdfab5ea19f45be60738e54754
RainPPR <PPR2125773894@163.com>
2024-11-23 13:22:36 +0800

    Create fedora.1.sh

---
+++ fedora.1.sh
@@ -0,0 +1,12 @@
+sudo sed -e 's|^metalink=|#metalink=|g' \ #
+         -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.ustc.edu.cn/fedora|g' \ #
+         -i.bak \ #
+         /etc/yum.repos.d/fedora.repo \ #
+         /etc/yum.repos.d/fedora-updates.repo #
+rpm-ostree upgrade #
+rpm-ostree install \ #
+	gcc gcc-c++ make cmake automake clang llvm lld lldb gdb \ #
+	yasm nasm ccache doxygen ctags cppcheck \ #
+	fpc ninja-build nodejs git unzip tar zip \ #
+	wget curl vim mpv vlc obs-studio libreoffice \ #
+	python3 pip python3-pip python3-ipython #
