5553d9242da2adda079a3ea9dd72111e2c052055
RainPPR <PPR2125773894@163.com>
2024-11-23 18:18:08 +0800

    Update ubuntu.live.sh

--- ubuntu.live.sh
+++ ubuntu.live.sh
@@ -1,3 +1,5 @@
+sudo touch /etc/apt/sources.list
+sudo touch /etc/apt/sources.list.d/ubuntu.sources
 sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sudo sed -i 's/http:/https:/g' /etc/apt/sources.list
