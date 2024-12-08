efd63e39a0bb770b455b962f38bf5f2729e7e4f5
RainPPR <41764766+RainPPR@users.noreply.github.com>
2024-12-08 16:58:33 +0800

    Update ubuntu.install.sh

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -1,3 +1,11 @@
+sudo touch /etc/apt/sources.list
+sudo touch /etc/apt/sources.list.d/ubuntu.sources
+sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+sudo sed -i 's/http:/https:/g' /etc/apt/sources.list
+sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list.d/ubuntu.sources
+sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/ubuntu.sources
+sudo sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt install -y \
@@ -7,5 +15,6 @@ sudo apt install -y \
 	ssh unzip tar zip wget curl vim git nodejs npm  \
 	python3 pip python3-pip ipython3 python-is-python3
 sudo apt autoremove -y
-
-# mpv vlc obs-studio libreoffice-common
+echo "Press enter to continue . . ."
+read -s
+sudo apt install -y mpv vlc obs-studio libreoffice-common
